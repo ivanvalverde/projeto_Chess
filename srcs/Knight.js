b3ee3592508c4922ec52ecb61d69714748098d3f
Ivan <ivanvalverde53@hotmail.com>
2020-07-12 21:19:59 -0300

    Inicio movimento cavalo

--- srcs/Knight.js
+++ srcs/Knight.js
@@ -11,12 +11,49 @@ class Knight {
         if(this._color == "black"){
             let blackKnight = document.createElement("img");
             blackKnight.src = "pngs/blackKnight.png";
+
+            blackKnight.addEventListener("click", function(){
+
+                if(whiteTurn == false){
+                    removeRedColorFromSquares();
+                    let number = parseInt(this.parentNode.id.split("square")[1]);
+                    console.log(number);
+                    let walk = [document.querySelector(`#square${number-17}`),
+                                document.querySelector(`#square${number-15}`),
+                                document.querySelector(`#square${number-10}`),
+                                document.querySelector(`#square${number-6}`),
+                                document.querySelector(`#square${number+6}`),
+                                document.querySelector(`#square${number+10}`),
+                                document.querySelector(`#square${number+15}`),
+                                document.querySelector(`#square${number+17}`)
+                                ];
+
+                    for (let i=0; i<8;i++){
+                        if(walk[i]==null){
+                            continue;
+                        } else{
+                            walk[i].classList.add("moveTo");
+                        elementToMove = this;
+                        }
+                        
+                    }
+                    blackKnight.parentNode.classList.remove("blackKnight");
+                    classToAdd = "blackKnight";
+                }
+
+            })
+
             blackKnight.classList.add("pieces");
             return blackKnight;
 
         } else{
             let whiteKnight = document.createElement("img");
             whiteKnight.src = "pngs/whiteKnight.png";
+
+            whiteKnight.addEventListener("click", function(){
+
+            })
+
             whiteKnight.classList.add("pieces");
             return whiteKnight;
         }
